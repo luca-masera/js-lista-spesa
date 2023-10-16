6ba3bc298961e47b638bc9ac984c916a7bcabe4e
luca-masera <lucamasera1995@gmail.com>
2023-10-16 16:53:57 +0200

    minestone 1

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,20 @@
 
 let utenteProdotti = prompt('Quanti prodotti vuoi acquistare?')
 console.log(utenteProdotti)
+const lista =[]
 
+let basta = false;
+let i = 0;
 
+while(i < lista.length){
+
+    lista.push (utenteProdotti)
+    console.log (lista)
+    
+    if(utenteProdotti === stop){
+        basta = true;
+    }
+    i++;
+
+}
+console.log (utenteProdotti)
